9d4426dc2cfa5f6e0c354eb44cac2d5807ec02e7
Linden Morgan <lmorgan2008@icloud.com>
2023-12-07 01:41:20 -0500

    added Main.py

---
+++ Main.py
@@ -0,0 +1,53 @@
+from openai import OpenAIAPI
+import requests
+from ics import Calendar
+
+#This defines the function to download the the webcal to an ics file
+def download_ics(webcal_link, ics_filename):
+    response = requests.get(webcal_link)
+    with open(ics_filename, 'wb') as file:
+        file.write(response.content)
+
+#This defines the funtion to convert the ics to txt
+def convert_ics_to_txt(ics_filename, txt_filename):
+    with open(ics_filename, 'r') as file:
+        calendar = Calendar(file.read())
+    with open(txt_filename, 'w') as file:
+        file.write(str(calendar))
+
+#Combines said functions 
+def webcal_to_txt(webcal_link, ics_filename, txt_filename):
+    download_ics(webcal_link, ics_filename)
+    convert_ics_to_txt(ics_filename, txt_filename)
+
+
+# Replace 'YOUR_API_KEY' with your actual OpenAI API key
+api_key = 'YOUR_API_KEY'
+
+client = OpenAIAPI(api_key)
+
+# Function to read content from a text file
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+# Specify the path to the text file
+text_file_path = "your_text_file.txt"
+
+# Read the content of the text file
+text_file_content = read_file(text_file_path)
+
+# Initialize conversation history with the content of the text file
+conversation_history = text_file_content
+
+# Create a chat completion using the OpenAI GPT-3.5-turbo model
+completion = client.ChatCompletion.create(
+    model="text-davinci-002",  # Use the appropriate model
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."},
+    ]
+)
+
+# Print the generated completion
+print(completion.choices[0].message['content'])
