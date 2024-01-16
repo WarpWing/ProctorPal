f5d11b2f186615f0fa5fbb18899fee4e19ff4030
Linden Morgan <lmorgan2008@icloud.com>
2024-01-16 08:41:11 -0500

    updated vector upload funtion

--- Qdrant/Qdrant.py
+++ Qdrant/Qdrant.py
@@ -3,16 +3,17 @@ from qdrant_client.models import VectorParams, Distance
 from Vectors.Hidden import Qdrant_API_KEY
 import numpy as np
 import json
+from Vectors.Vectors import embedded_query
+from Vectors.Vectors import text2
+
+client = QdrantClient("localhost", port=6333)
 
 qdrant_client = QdrantClient(
     "https://29b7abb9-dd18-450c-8e2b-706de38ba80b.us-east4-0.gcp.cloud.qdrant.io:6333",
     api_key=Qdrant_API_KEY,
 )
 
-fd = open('./startups.json')
 
-# payload is now an iterator over startup data
-payload = map(json.loads, fd)
 
 # Here we load all vectors into memory, numpy array works as iterable for itself.
 # Other option would be to use Mmap, if we don't want to load all data into RAM
@@ -20,8 +21,8 @@ vectors = np.load('./startup_vectors.npy')
 
 qdrant_client.upload_collection(
     collection_name='startups',
-    vectors=vectors,
-    payload=payload,
+    vectors=embedded_query,
+    payload=text2,
     ids=None,  # Vector ids will be assigned automatically
     batch_size=256  # How many vectors will be uploaded in a single request?
 )
--- Vectors/Vectors.py
+++ Vectors/Vectors.py
@@ -3,11 +3,11 @@ from qdrant_client import QdrantClient
 from qdrant_client.http.models import PointStruct
 import numpy as np
 from Hidden import YOUR_API_KEY
-import spacy
-spacy.load("en_core_web_sm")
-from langchain.text_splitter import SpacyTextSplitter
+from langchain_openai import OpenAIEmbeddings
 
 
+
+embeddings_model = OpenAIEmbeddings(openai_api_key=YOUR_API_KEY)
 
 
 n = 0
@@ -15,28 +15,11 @@ n = 0
 openai.api_key = YOUR_API_KEY
 with open('Vectors/handbook_output.txt', 'r') as f:
     text = f.read()
-    text_splitter = SpacyTextSplitter()
-    text2 = text_splitter.split_text(text)
+    text2 = text.split(".")
 
 while(n != len(text2)):
 
-    cache = text2[n]
+    embedded_query = embeddings_model.embed_query(text2[n])
+    embedded_query[:5]
 
-    response = openai.Embedding.create(
-        input=cache,
-        model="text-embedding-ada-002" # Imagine not using davinci but ok bro lol
-    )
-
-    embeddings = response['data'][0]['embedding'] # get the actual embeddings
-    embeddings_array = np.array(embeddings) # turn them into an actual Numpy Array
-    qdrant_client = QdrantClient(host='localhost', port=6333) #qdrant client connection
-    points = [PointStruct(id=i, vector=vec.tolist()) for i, vec in enumerate(embeddings_array)] # change them to vector embeddings
-
-    collection_name = 'procciepal' # Vector Collection name
-    qdrant_client.upsert(
-        collection_name=collection_name,
-        points=points
-    )
-    n += 1
-     
-    
+n += 1
